c224e828382582cdd93703ea57175094b73071c9
MAX-HACKER <anusas860@gmail.com>
2026-02-21 05:36:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    base: '/bloomher/',   // ← change 'bloomher' to your exact GitHub repo name
+    base: '/',
 })
